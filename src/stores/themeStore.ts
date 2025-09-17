04d64b7065d549442074832cad1940ff1c9e9bc7
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2025-09-17 01:25:40 -0600

    renamed theme idigo to mdb

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -3,14 +3,14 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 type ThemeState = {
-  theme: 'indigo' | 'green';
-  setTheme: (theme: 'indigo' | 'green') => void;
+  theme: 'mdb' | 'green';
+  setTheme: (theme: 'mdb' | 'green') => void;
 };
 
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
-      theme: 'indigo', // Tema por defecto
+      theme: 'mdb', // Tema por defecto
       setTheme: (theme) => set({ theme }),
     }),
     {
