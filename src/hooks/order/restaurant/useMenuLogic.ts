001bf41d66e4f0d0e00b19fa3dfd94d49f844fd6
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2026-03-12 04:41:41 -0600

    Alphablet succes

--- src/hooks/order/restaurant/useMenuLogic.ts
+++ src/hooks/order/restaurant/useMenuLogic.ts
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 export function useMenuLogic() {
   const { activeConfig } = useConfigStore()
   const { isLoaded, loading, _hasHydrated, getRestauranMenu} = restauranMenuStore()
-  const orderIcon =  activeConfig && activeConfig.includes("restaurant-sales-order-menu-alp") ? "alphabet" : "-order";
+  const orderIcon =  activeConfig && activeConfig.includes("restaurant-sales-order-menu-alp") ? "-alphabet" : "-order";
 
   useEffect(() => {
     if (_hasHydrated && !isLoaded && !loading) {
